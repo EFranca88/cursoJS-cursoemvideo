4fdab27f111c77da66d633f108666379b3910ee3
Eric França <73256869+EFranca88@users.noreply.github.com>
2025-03-21 00:15:09 -0300

    Adiciona exercício Tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,6 +1,14 @@
-var contar = "0"
-var end = "5"
-while (contar <= end) {
-console.log(` ${contar} &#x1F449;`)
-contar++
+function calcular() {
+    var baseCalc = document.getElementById('ntable')
+    var restabuada = document.getElementById('tabuada')
+    restabuada.innerHTML = ``
+    
+    if (baseCalc.value === '') {
+        restabuada.innerHTML = `<strong>Impossível calcular!</strong> <br/> Por favor, preencha o campo <strong>Número</strong> antes de gerar a tabuada.`
+    } else {
+        for (let i = 1; i <= 10; i++) {
+            let multiplicar = Number(baseCalc.value) * i
+            restabuada.innerHTML += `${baseCalc.value} x ${i} = ${multiplicar} <br/>`
+        } 
+    }
 }
