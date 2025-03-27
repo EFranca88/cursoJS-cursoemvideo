adf10606ec331bb2c43f3ebf0a164b194613ed6f
Eric França <73256869+EFranca88@users.noreply.github.com>
2025-03-27 17:22:20 -0300

    Adiciona leve camada de validação no exercício ex018

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -3,6 +3,12 @@ var myArray = [],
     
 function numberTasks() { 
     let sel = document.getElementById('selAdd'),
+        cont = document.getElementById('cont'),
+        maior = document.getElementById('maior'),
+        menor = document.getElementById('menor'),
+        soma = document.getElementById('soma'),
+        media = document.getElementById('media'),
+        ultimo = document.getElementById('ultimo'),
         nAdd = Number(number.value)
 
     if ( nAdd <= 0  || nAdd >= 101 || myArray.indexOf(nAdd) != -1) {
@@ -17,6 +23,12 @@ function numberTasks() {
             item.text = `Valor ${nAdd} adicionado.`
             item.value = `tab${index}`
             sel.appendChild(item)
+            cont.innerHTML = ''
+            maior.innerHTML = ''
+            menor.innerHTML = ''
+            soma.innerHTML = ''
+            media.innerHTML = ''
+            ultimo.innerHTML = ''
         } else {
             sel.innerHTML = ''
             item.text = `Valor ${nAdd} adicionado.`
@@ -27,38 +39,42 @@ function numberTasks() {
 }
 
 function contagemFinal() {
-    let cont = document.getElementById('cont'),
-        maior = document.getElementById('maior'),
-        menor = document.getElementById('menor'),
-        soma = document.getElementById('soma'),
-        media = document.getElementById('media'),
-        ultimo = document.getElementById('ultimo'),
-        arrayCont = myArray.length,
-        nMaior = 1, nMenor = 100, nSoma = 0, nMedia = 0 
+    if (myArray.length != 0) {
+        let cont = document.getElementById('cont'),
+            maior = document.getElementById('maior'),
+            menor = document.getElementById('menor'),
+            soma = document.getElementById('soma'),
+            media = document.getElementById('media'),
+            ultimo = document.getElementById('ultimo'),
+            arrayCont = myArray.length,
+            nMaior = 0, nMenor = 100, nSoma = 0, nMedia = 0 
         
-    for (let index = 0; index < arrayCont; index++) {
-        const element = myArray[index];
-        if (element > nMaior) {
-            nMaior = element   
+        for (let index = 0; index < arrayCont; index++) {
+            const element = myArray[index];
+            if (element > nMaior) {
+                nMaior = element   
+            }
         }
-    }
-    for (let index = 0; index < arrayCont; index++) {
-        const element = myArray[index];
-        if (element < nMenor) {
-            nMenor = element   
+        for (let index = 0; index < arrayCont; index++) {
+            const element = myArray[index];
+            if (element < nMenor) {
+                nMenor = element   
+            }
         }
-    }
-    for (let index = 0; index < arrayCont; index++) {
-        const element = myArray[index];
-        nSoma += element
-    }
+        for (let index = 0; index < arrayCont; index++) {
+            const element = myArray[index];
+            nSoma += element
+        }
 
-    nMedia = nSoma / arrayCont
+        nMedia = nSoma / arrayCont
 
-    cont.innerHTML = `Ao todo temos ${arrayCont} números cadastrados`
-    maior.innerHTML = `O maior valor informado foi ${nMaior}`
-    menor.innerHTML = `O menor valor informado foi ${nMenor}`
-    soma.innerHTML = `Somando todos os valores, temos ${nSoma}`
-    media.innerHTML = `A média dos valores digitados é ${nMedia}`
-    ultimo.innerHTML = `O ultimo valor do array foi ${myArray[myArray.length - 1]}`
+        cont.innerHTML = `Ao todo temos ${arrayCont} números cadastrados`
+        maior.innerHTML = `O maior valor informado foi ${nMaior}`
+        menor.innerHTML = `O menor valor informado foi ${nMenor}`
+        soma.innerHTML = `Somando todos os valores, temos ${nSoma}`
+        media.innerHTML = `A média dos valores digitados é ${nMedia}`
+        ultimo.innerHTML = `O ultimo valor do array foi ${myArray[myArray.length - 1]}`
+    } else {
+        alert('Adicionar valor entre 1 e 100 antes de finalizar!')
+    }    
 }
